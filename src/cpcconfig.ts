25e62e1b9e1086accc9c72742c9dc0115737a7e3
Benchmarko <benchmarko@users.noreply.github.com>
2022-07-20 02:46:20 +0200

    config for server restored

--- src/cpcconfig.ts
+++ src/cpcconfig.ts
@@ -1,6 +1,6 @@
 /* cpcconfig.ts - configuration file for CPCBasicTS */
 
 export const cpcconfig = { // eslint-disable-line no-unused-vars
-	databaseDirs: "../../CPCBasic/examples,../../CPCBasicApps/apps,./examplesTS,storage" // local test
-	//databaseDirs: "https://benchmarko.github.io/CPCBasic/examples,https://benchmarko.github.io/CPCBasicApps/apps,./examplesTS,storage"
+	//databaseDirs: "../../CPCBasic/examples,../../CPCBasicApps/apps,./examplesTS,storage" // local test
+	databaseDirs: "https://benchmarko.github.io/CPCBasic/examples,https://benchmarko.github.io/CPCBasicApps/apps,./examplesTS,storage"
 };
