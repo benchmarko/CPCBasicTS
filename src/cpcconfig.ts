77d3914338113b9fc9945e601b1e58b737310925
Benchmarko <benchmarko@users.noreply.github.com>
2026-06-12 17:24:56 +0200

    restore config for server URL again

--- src/cpcconfig.ts
+++ src/cpcconfig.ts
@@ -1,8 +1,8 @@
 /* cpcconfig.ts - configuration file for CPCBasicTS */
 
 export const cpcconfig = { // eslint-disable-line no-unused-vars
-	//databaseDirs: "./examples,https://benchmarko.github.io/CPCBasicApps/apps,https://benchmarko.github.io/LocoBasic/examples=locobasic,https://benchmarko.github.io/CPCBasicApps/rosetta,storage",
-	databaseDirs: "./examples,../../CPCBasicApps/apps,../../LocoBasic/dist/examples=locobasic,../../CPCBasicApps/rosetta,storage", // local test
+	databaseDirs: "./examples,https://benchmarko.github.io/CPCBasicApps/apps,https://benchmarko.github.io/LocoBasic/examples=locobasic,https://benchmarko.github.io/CPCBasicApps/rosetta,storage",
+	//databaseDirs: "./examples,../../CPCBasicApps/apps,../../LocoBasic/dist/examples=locobasic,../../CPCBasicApps/rosetta,storage", // local test
 
 	// just an example, not the full list of moved examples...
 	redirectExamples: {
